ac0b9571913ad27627ac62624ac561ebaa817fec
Alexandre-Furstenberger <alexandre.furstenberger@gmail.com>
2018-05-22 14:27:12 -0300

    FEAT: Add custom markers and map layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-bootstrap'
+import {Button, Grid, Col, Row, PageHeader, FormControl} from 'react-bootstrap'
 import MapContainer from './MapContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button bsStyle="default">Default Button</Button>
-        <MapContainer />
+      <div>
+        <PageHeader className="text-center">Neighborhood Map App</PageHeader>
+        <Grid fluid>
+          <Row className="show-grid">      
+            <Col xs={3} sm={3} md={3} lg={3} className="fill">
+              <form>
+                <FormControl
+                  id="formControlsText"
+                  type="text"
+                  label="Text"
+                  placeholder="Search ex: Restaurants, etc"
+                />
+              </form>
+              <Button bsStyle="default">Default Button</Button>
+            </Col>
+            <Col xs={9} sm={9} md={9} lg={9} className="fill">
+              <MapContainer />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends Component {
-    render() {
-        return (
-            <Map google={this.props.google} zoom={14}>
 
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
+    render() {
+
+        const style = {
+            height: '100%'
+        }
+
+        return (
+            <Map google={this.props.google} 
+                style={style}
+                initialCenter={{ lat: -23.646156, lng: - 46.669538}}
+                zoom={15}
+                >
+                <Marker
+                    onClick={this.onMarkerClick}
+                    name={'Current location'}
+                    position={{ lat: -23.646156, lng: - 46.669538 }}
+                    icon={{
+                        url: "marker.svg",
+                        anchor: new this.props.google.maps.Point(16, 16),
+                        scaledSize: new this.props.google.maps.Size(32, 32)
+                    }} />
             </Map>
         );
     }
